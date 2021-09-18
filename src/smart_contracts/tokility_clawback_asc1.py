7c0be92244e99fae0c2dac6011188c6202723b24
DESKTOP-0FNBS57\Vilijan <monevvilijan@gmail.com>
2021-09-18 15:27:28 +0200

    updated stateless asc1.

--- src/smart_contracts/tokility_clawback_asc1.py
+++ src/smart_contracts/tokility_clawback_asc1.py
@@ -13,39 +13,17 @@ class TokilityClawbackASC1:
 
     def initial_buy(self):
         """
-        Arguments:
-        - app_method_name: str
-
-        Foreign assets:
-        - asa_id - the ID of the asa that the user wants to buy.
-
         Atomic Transfer:
-        1. Application call.
-        2. Payment from buyer to ASA_CREATOR.
-        3. Asset transfer from Clawback to buyer.
-
-        :return:
+        0. Application call.
+        1. Payment from buyer to ASA_CREATOR.
+        2. Asset transfer from Clawback to buyer.
         """
 
         return Seq([
-            Assert(Global.group_size() == Int(3)),
-            Assert(Gtxn[0].application_args.length() == Int(1)),
-            # Valid first transaction
-            Assert(Gtxn[0].type_enum() == TxnType.ApplicationCall),
-            Assert(Gtxn[0].sender() == Gtxn[1].sender()),
+            Assert(Gtxn[1].fee() <= Int(1000)),
+            Assert(Gtxn[1].close_remainder_to() == Global.zero_address()),
+            Assert(Gtxn[1].rekey_to() == Global.zero_address()),
 
-            # Valid second transaction.
-            Assert(Gtxn[1].type_enum() == TxnType.Payment),
-            Assert(Gtxn[1].receiver() == Addr(self.configuration.asa_owner_address)),
-            Assert(Gtxn[1].amount() == Int(self.configuration.initial_offering_configuration.asa_price)),
-
-            # Valid third transaction.
-            Assert(Gtxn[2].type_enum() == TxnType.AssetTransfer),
-            Assert(Gtxn[2].xfer_asset() == Int(self.configuration.asa_id)),
-            Assert(Gtxn[2].asset_receiver() == Gtxn[0].sender()),
-
-            # TODO: Later on this should be allowed to more instances to be bought at once.
-            Assert(Gtxn[2].asset_amount() == Int(1)),
             Assert(Gtxn[2].fee() <= Int(1000)),
             Assert(Gtxn[2].asset_close_to() == Global.zero_address()),
             Assert(Gtxn[2].rekey_to() == Global.zero_address()),
@@ -55,52 +33,32 @@ class TokilityClawbackASC1:
 
     def buy_from_seller(self):
         """
-        Arguments:
-        - app_method_name: str
-
-        Foreign assets:
-        - asa_id - the ID of the asa that the user wants to buy.
-
-        Accounts:
-        - seller_account - the account address of the seller.
-
         Atomic Transfer:
-        1. Application call.
-        2. Payment from buyer to ASA_CREATOR. (fees are paid to the asa_creator)
-        3. Payment from buyer to ASA_SELLER. (the actual price of the ASA on the asa offer)
+        0. Application call.
+        1. Payment from buyer to ASA_CREATOR. (creator fee)
+        2. Payment from buyer to ASA_SELLER. (the actual price of the ASA on the asa offer)
+        3. Payment from buyer to PLATFORM_ADDRESS. (platform fee)
         4. Asset transfer from Clawback to buyer.
 
         :return:
         """
 
         return Seq([
-            Assert(Global.group_size() == Int(4)),
-            Assert(Gtxn[0].application_args.length() == Int(1)),
-            # Valid first transaction.
-            Assert(Gtxn[0].type_enum() == TxnType.ApplicationCall),
-            Assert(Gtxn[0].sender() == Gtxn[1].sender()),
-            Assert(Gtxn[0].sender() == Gtxn[2].sender()),
+            Assert(Gtxn[1].fee() <= Int(1000)),
+            Assert(Gtxn[1].close_remainder_to() == Global.zero_address()),
+            Assert(Gtxn[1].rekey_to() == Global.zero_address()),
 
-            # Valid second transaction.
-            Assert(Gtxn[1].type_enum() == TxnType.Payment),
-            Assert(Gtxn[1].receiver() == Addr(self.configuration.asa_owner_address)),
-            Assert(Gtxn[1].amount() == Int(self.configuration.economy_configuration.owner_fee)),
-
-            # # Valid third transaction.
-            Assert(Gtxn[2].type_enum() == TxnType.Payment),
-            Assert(Gtxn[2].amount() <= Int(self.configuration.economy_configuration.max_sell_price)),
-            # The amount and the receiver of this transaction is validated in the dex application.
-
-            # # Valid forth transaction.
-            Assert(Gtxn[3].type_enum() == TxnType.AssetTransfer),
-            Assert(Gtxn[3].xfer_asset() == Int(self.configuration.asa_id)),
-            Assert(Gtxn[3].asset_receiver() == Gtxn[0].sender()),
-
-            # TODO: Later on this should be allowed to more instances to be bought at once.
-            Assert(Gtxn[3].asset_amount() == Int(1)),
-            Assert(Gtxn[3].fee() <= Int(1000)),
-            Assert(Gtxn[3].asset_close_to() == Global.zero_address()),
+            Assert(Gtxn[2].fee() <= Int(1000)),
+            Assert(Gtxn[2].close_remainder_to() == Global.zero_address()),
             Assert(Gtxn[3].rekey_to() == Global.zero_address()),
+
+            Assert(Gtxn[3].fee() <= Int(1000)),
+            Assert(Gtxn[3].close_remainder_to() == Global.zero_address()),
+            Assert(Gtxn[3].rekey_to() == Global.zero_address()),
+
+            Assert(Gtxn[4].fee() <= Int(1000)),
+            Assert(Gtxn[4].asset_close_to() == Global.zero_address()),
+            Assert(Gtxn[4].rekey_to() == Global.zero_address()),
 
             Return(Int(1))
         ])
@@ -118,33 +76,24 @@ class TokilityClawbackASC1:
         - asa_id - the ID of the asa that the user wants to gift.
 
         Atomic Transfer:
-        1. Application call.
-        2. Payment from ASA_OWNER to ASA_CREATOR. (fees are paid to the asa_creator)
+        0. Application call.
+        1. Payment from ASA_OWNER to ASA_CREATOR. (fees are paid to the asa_creator)
+        2. Payment from ASA_OWNER to PLATFORM_ADDRESS.
         3. Asset transfer from Clawback to GIFT_ADDRESS.
 
         """
         return Seq([
-            Assert(Global.group_size() == Int(3)),
-            Assert(Gtxn[0].application_args.length() == Int(2)),
-            # Valid first transaction
-            Assert(Gtxn[0].type_enum() == TxnType.ApplicationCall),
-            Assert(Gtxn[0].sender() == Gtxn[1].sender()),
+            Assert(Gtxn[1].fee() <= Int(1000)),
+            Assert(Gtxn[1].close_remainder_to() == Global.zero_address()),
+            Assert(Gtxn[1].rekey_to() == Global.zero_address()),
 
-            # Valid second transaction.
-            Assert(Gtxn[1].type_enum() == TxnType.Payment),
-            Assert(Gtxn[1].receiver() == Addr(self.configuration.asa_owner_address)),
-            Assert(Gtxn[1].amount() == Int(self.configuration.economy_configuration.owner_fee)),
-
-            # Valid third transaction.
-            Assert(Gtxn[2].type_enum() == TxnType.AssetTransfer),
-            Assert(Gtxn[2].xfer_asset() == Int(self.configuration.asa_id)),
-            Assert(Gtxn[2].asset_receiver() == Gtxn[0].application_args[1]),
-
-            # TODO: Later on this should be allowed to more instances to be bought at once.
-            Assert(Gtxn[2].asset_amount() == Int(1)),
             Assert(Gtxn[2].fee() <= Int(1000)),
-            Assert(Gtxn[2].asset_close_to() == Global.zero_address()),
-            Assert(Gtxn[2].rekey_to() == Global.zero_address()),
+            Assert(Gtxn[2].close_remainder_to() == Global.zero_address()),
+            Assert(Gtxn[3].rekey_to() == Global.zero_address()),
+
+            Assert(Gtxn[3].fee() <= Int(1000)),
+            Assert(Gtxn[3].asset_close_to() == Global.zero_address()),
+            Assert(Gtxn[3].rekey_to() == Global.zero_address()),
 
             Return(Int(1))
         ])
